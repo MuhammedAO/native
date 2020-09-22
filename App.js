4e0b1d86a002a75a9124e3a3b37fc5f9b1c7479f
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-09-22 16:28:44 +0100

    View,Text,Style

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import { StatusBar } from 'expo-status-bar'
+// import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello World</Text>
+        <Text style={styles.boldText}>Hello World</Text>
+        <Text style={styles.boldText}>Hello World</Text>
+        <Text style={styles.boldText}>Hello World</Text>
+        <Text style={styles.boldText}>Hello World</Text>
+      </View>
     </View>
   )
 }
@@ -18,4 +23,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'green',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold'
+  }
 })
