cd7750965f67d08d20babdcd1a5482d39ab409fb
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-09-23 21:31:50 +0100

    FlatList Component

--- App.js
+++ App.js
@@ -1,29 +1,37 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, View, ScrollView } from 'react-native'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
 
 export default function App() {
   const [students, setStudents] = useState([
-    { name: 'student-1', key: '1' },
-    { name: 'student-2', key: '2' },
-    { name: 'student-3', key: '3' },
-    { name: 'student-4', key: '4' },
-    { name: 'student-5', key: '5' },
-    { name: 'student-6', key: '6' },
-    { name: 'student-7', key: '7' },
-    { name: 'student-8', key: '8' },
+    { name: 'student-1', id: '1' },
+    { name: 'student-2', id: '2' },
+    { name: 'student-3', id: '3' },
+    { name: 'student-4', id: '4' },
+    { name: 'student-5', id: '5' },
+    { name: 'student-6', id: '6' },
+    { name: 'student-7', id: '7' },
+    { name: 'student-8', id: '8' },
   ])
 
   return (
     <View style={styles.container}>
-      <ScrollView>
-        {
-          students.map(student => (
-            <View key={student.key}>
-              <Text style={styles.item}>{student.name}</Text>
-            </View>
-          ))
-        }
-      </ScrollView>
+      {/*
+      the data prop specifies the data that we want to output
+      when u use a flatlist, if we have numerous items to display,
+      all of the items do not automatically load unto the screen when it first renders.
+      only the first few loads and more will follow as u scroll
+      the flatlist automatically looks for the key property on item which might differ because most data
+      come with an id prop
+      */}
+      <FlatList
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+        data={students}
+        renderItem={({ item }) => (
+          <Text style={styles.item}>{item.name}</Text>
+        )}
+      />
+
     </View>
   )
 }
@@ -40,7 +48,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     marginTop: 24,
     padding: 30,
-    backgroundColor: 'tomato',
+    backgroundColor: 'pink',
     fontSize: 24,
   },
 })
