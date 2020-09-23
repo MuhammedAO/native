260642a1cf62a92ae3d52e625648490d7c1a388b
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-09-23 22:20:38 +0100

    Touchable components

--- App.js
+++ App.js
@@ -1,37 +1,31 @@
-import React, { useState } from 'react'
-import { StyleSheet, Text, View, FlatList } from 'react-native'
+import React, { useState } from "react"
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
 
-export default function App() {
-  const [students, setStudents] = useState([
-    { name: 'student-1', id: '1' },
-    { name: 'student-2', id: '2' },
-    { name: 'student-3', id: '3' },
-    { name: 'student-4', id: '4' },
-    { name: 'student-5', id: '5' },
-    { name: 'student-6', id: '6' },
-    { name: 'student-7', id: '7' },
-    { name: 'student-8', id: '8' },
-  ])
+const App = () => {
+  const [count, setCount] = useState(0)
+  {/*in react, whenever our next value of state depends on the prevState, 
+  it's safer to pass a fn which returns the new state
+ */}
+  const onPressIncrease = () => setCount(prevCount => prevCount + 1)
+  const onPressDecrease = () => setCount(prevCount => prevCount - 1)
 
   return (
     <View style={styles.container}>
-      {/*
-      the data prop specifies the data that we want to output
-      when u use a flatlist, if we have numerous items to display,
-      all of the items do not automatically load unto the screen when it first renders.
-      only the first few loads and more will follow as u scroll
-      the flatlist automatically looks for the key property on item which might differ because most data
-      come with an id prop
-      */}
-      <FlatList
-        numColumns={2}
-        keyExtractor={(item) => item.id}
-        data={students}
-        renderItem={({ item }) => (
-          <Text style={styles.item}>{item.name}</Text>
-        )}
-      />
-
+      <View style={styles.countContainer}>
+        <Text>Count: {count}</Text>
+      </View>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={onPressIncrease}
+      >
+        <Text>Count up</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.btn}
+        onPress={onPressDecrease}
+      >
+        <Text>Count Down</Text>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -39,16 +33,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 40,
-    paddingHorizontal: 20,
-    backgroundColor: '#fff',
+    justifyContent: "center",
+    paddingHorizontal: 10
   },
-  item: {
-    flex: 1,
-    marginHorizontal: 10,
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: 'pink',
-    fontSize: 24,
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
   },
+  countContainer: {
+    alignItems: "center",
+    padding: 10
+  },
+  btn: {
+    marginTop: 15,
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  }
 })
+
+export default App
